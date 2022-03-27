442cd0b08de4745f69f80ac7e22fcd8a39f149be
Alamin <al.ahmed@outlook.com>
2022-03-27 00:28:50 +0000

    added cors

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 let express = require('express');
 let path = require('path');
 let logger = require('morgan');
+let cors = require('cors')
 let indexRouter = require('./routes/index');
 let errorHandler = require('./middleware/error-handler.js');
 
 let app = express();
 
+/** Add middleware */
+// Enables cors.
+app.use(cors());
+app.options('*', cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+/** Add router */
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
